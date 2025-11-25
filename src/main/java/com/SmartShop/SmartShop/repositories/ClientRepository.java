21d004912aa71646a2ef1d6ede9fe904ffb5f86f
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-11-25 21:56:46 +0100

    Add ClientRepository interface for managing Client entities

---
+++ src/main/java/com/SmartShop/SmartShop/repositories/ClientRepository.java
@@ -0,0 +1,13 @@
+package com.SmartShop.SmartShop.repositories;
+
+import com.SmartShop.SmartShop.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+    boolean existsByEmail(String email);
+    Optional<Client> findByEmail(String email);
+}
