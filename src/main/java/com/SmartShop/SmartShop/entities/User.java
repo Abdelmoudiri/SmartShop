3da29f0f874ca40760348b855330875035495e20
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-11-25 13:03:41 +0100

    Refactor User entity: change id type to Long, update role to use UserRole enum, and ensure unique username constraint

--- src/main/java/com/SmartShop/SmartShop/entities/User.java
+++ src/main/java/com/SmartShop/SmartShop/entities/User.java
@@ -1,18 +1,34 @@
 package com.SmartShop.SmartShop.entities;
 
 
+import com.SmartShop.SmartShop.entities.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User{
-
-
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    priva String username;
+    @Column(nullable = false,unique = true)
+    private String username;
 
+    @Column(nullable = false)
     private String password;
 
     @Enumerated(EnumType.STRING)
-    private Role role;
+    @Column(nullable = false)
+    private UserRole role;
 
+    @OneToOne(mappedBy = "user" , cascade = CascadeType.ALL)
+    private Client clientDetails;
 }
