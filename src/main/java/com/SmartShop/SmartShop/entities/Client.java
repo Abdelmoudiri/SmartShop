d68c5a44cd3c2727b86883adcdbf0fd678a6ba39
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-11-25 13:03:48 +0100

    Refactor Client entity: change id type to Long, rename name to nom, and update fidelite field to use CustomerTier enum

--- src/main/java/com/SmartShop/SmartShop/entities/Client.java
+++ src/main/java/com/SmartShop/SmartShop/entities/Client.java
@@ -1,18 +1,45 @@
-package com.SmartShop.SmartShop.entities.enums;
+package com.SmartShop.SmartShop.entities;
 
 
+import com.SmartShop.SmartShop.entities.enums.CustomerTier;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.Mapping;
+
+import java.util.List;
+
+@Entity
+@Table(name="clients")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class Client {
 
-
     @Id
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    private String name;
+    @Column(nullable = false)
+    private String nom;
 
+    @Column(nullable = false, unique = true)
     private String email;
-    
+
+    @OneToOne
+    @JoinColumn(name = "user_id",referencedColumnName = "id")
+    private User user;
+
     @Enumerated(EnumType.STRING)
-    private Niveu niveu;
+    @Builder.Default
+    private CustomerTier fidelite=CustomerTier.BASIC;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders;
+
 
 }
