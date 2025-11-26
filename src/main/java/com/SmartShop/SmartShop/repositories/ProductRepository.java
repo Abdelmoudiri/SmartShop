ae33efbd87889774446ce31de2507a3ee2534c9b
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-11-26 09:56:16 +0100

    Add ProductRepository interface for managing Product entities

---
+++ src/main/java/com/SmartShop/SmartShop/repositories/ProductRepository.java
@@ -0,0 +1,18 @@
+package com.SmartShop.SmartShop.repositories;
+
+import com.SmartShop.SmartShop.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+    List<Product> findByDeletedFalse();
+
+    Page<Product> findByDeletedFalse(Pageable pageable);
+}
