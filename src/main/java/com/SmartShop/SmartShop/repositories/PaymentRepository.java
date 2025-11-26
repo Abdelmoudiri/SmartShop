fe70f4cefbc6135084bd08677085099a435b5538
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-11-26 09:56:28 +0100

    Add PaymentRepository interface for managing Payment entities

---
+++ src/main/java/com/SmartShop/SmartShop/repositories/PaymentRepository.java
@@ -0,0 +1,13 @@
+package com.SmartShop.SmartShop.repositories;
+
+import com.SmartShop.SmartShop.entities.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    List<Payment> findByOrderId(Long orderId);
+}
