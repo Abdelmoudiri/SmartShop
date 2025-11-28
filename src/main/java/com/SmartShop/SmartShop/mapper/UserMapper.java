91dd528295e2db49fb72902093b26533aa6d033a
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-11-28 09:06:31 +0100

    Add UserMapper for converting between User entity and AuthDTO

---
+++ src/main/java/com/SmartShop/SmartShop/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.SmartShop.SmartShop.mapper;
+
+
+import com.SmartShop.SmartShop.dto.AuthDTO;
+import com.SmartShop.SmartShop.entities.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+   User toEntity(AuthDTO.RegisterRequest request);
+   AuthDTO.RegisterRequest toResponse(User user);
+
+}
